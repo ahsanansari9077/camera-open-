3d391f21e3c83093402c35463c6d971e38f5e727
ahsanansari9077 <61463845+ahsanansari9077@users.noreply.github.com>
2020-02-26 03:05:33 -0800

    Add files via upload

---
+++ camera open/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  camera open
+//
+//  Created by AHSAN on 20/02/2020.
+//  Copyright © 2020 AHSAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func openGallery(_ sender: Any) {
+        let library = UIImagePickerController()
+        library.delegate = self
+        library.sourceType = .photoLibrary
+        self.present(library, animated: true)
+    }
+    
+    
+    
+}
+
